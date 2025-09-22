94b443ab13236e4dba8d208aa0bbbcdd0d4985e8
caiosid <caiosidartha8@gmail.com>
2025-09-22 16:24:19 -0300

    Choose Language

--- lexilive-project/LexiLive/src/screens/ChooseLanguage/ChooseLanguage.jsx
+++ lexilive-project/LexiLive/src/screens/ChooseLanguage/ChooseLanguage.jsx
@@ -1,16 +1,53 @@
-import { View, Text} from "react-native";
+import { View, Text, TouchableOpacity, Image } from "react-native";
 import { styles } from "./styles";
 
 export default function ChooseLanguage() {
   return (
     <View style={styles.container}>
-        <Text style={styles.title}>Escolha o seu idioma</Text>
-        <Text style={styles.subtitle}>Selecione um idioma para começar a aprender</Text>
+      <Text style={styles.title}>Selecione o seu idioma</Text>
+      <Text style={styles.subTitle}>Idiomas</Text>
 
-        <View>
-            <Text>oi</Text>
-        </View>
+      <View>
+        <TouchableOpacity style={styles.languageButton}>
+          <Image
+            source={require("../../assets/united-state.png")}
+            style={styles.flagImage}
+          />
+          <Text style={styles.languageText}>English</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.languageButton}>
+          <Image
+            source={require("../../assets/france.png")}
+            style={styles.flagImage}
+          />
+          <Text style={styles.languageText}>French</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.languageButton}>
+          <Image
+            source={require("../../assets/italy.png")}
+            style={styles.flagImage}
+          />
+          <Text style={styles.languageText}>Italian</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.languageButton}>
+          <Image
+            source={require("../../assets/germany.png")}
+            style={styles.flagImage}
+          />
+          <Text style={styles.languageText}>German</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.languageButton}>
+          <Image
+            source={require("../../assets/spain.png")}
+            style={styles.flagImage}
+          />
+          <Text style={styles.languageText}>Spanish</Text>
+        </TouchableOpacity>
+      </View>
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.titleButton}>Começar</Text>
+      </TouchableOpacity>
     </View>
-    
   );
 }
